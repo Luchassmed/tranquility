c28758872e0f87ee56508f995adcf9c9a1fe47ac
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2022-11-07 23:14:07 +0100

    feat: nav dropdown

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
 import Portfolio from "../components/Portfolio";
+import Socials from "../components/Socials";
 
 export default function Home() {
   return (
     <>
+      <Socials />
       <Hero />
       <Portfolio />
     </>

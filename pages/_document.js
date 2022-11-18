2fa5a80c54d13791084ed14e89cf00898d80df1e
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2022-11-18 16:06:37 +0100

    docs

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/zsh7xbq.css" />
+        <title>Luchas Schmidt</title>
       </Head>
       <body>
         <Main />

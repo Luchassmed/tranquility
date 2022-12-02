acd6b52eef8740a34d9b0967db0269b94626634f
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2022-12-02 19:26:04 +0100

    style: md

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -35,7 +35,7 @@ export default function Portfolio({ repos }) {
     <div>
       <div className="md:grid grid-cols-3 ">
         <div className="bg-white">
-          <div className="flex flex-row pl-10 py-12 lg:pl-40 2xl:pl-72 xl:py-24">
+          <div className="flex flex-row pl-8 py-12 lg:pl-40 2xl:pl-72 xl:py-24">
             <div ref={sliderRef} className="keen-slider pb-10">
               {repos.map((repo, i) => {
                 return (
@@ -64,7 +64,7 @@ export default function Portfolio({ repos }) {
           </div>
         </div>
         <div className="bg-white md:py-12 flex items-center md:block">
-          <div className="flex px-10 md:justify-end lg:px-24 pb-8 xl:pb-4 md:pb-4 lg:pb-4 xl:pt-12">
+          <div className="flex px-8 md:justify-end lg:px-24 pb-8 xl:pb-4 md:pb-4 lg:pb-4 xl:pt-12">
             <h3 className="text-[#003367] font-bold">
               Portfolio{" "}
               <span className="text-[#8C8F99] font-normal">carousel:</span>
@@ -99,7 +99,7 @@ export default function Portfolio({ repos }) {
             </motion.h3>
           </div>
         </div>
-        <div className="bg-[#5e6ad2] py-12 lg:py-0 xl:py-0 px-10 xl:px-32">
+        <div className="bg-[#5e6ad2] py-12 lg:py-0 xl:py-0 px-8 xl:px-32">
           <div ref={sliderRefSecond} className="keen-slider">
             {websites.map((website, i) => {
               return (
@@ -122,12 +122,12 @@ export default function Portfolio({ repos }) {
               );
             })}
           </div>
-          <div className="flex flex-row items-center gap-16">
+          <div className="flex flex-row items-center gap-12 md:gap-16">
             <p className="font-bold"> Websites I&apos;ve made, carousel:</p>
 
             <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
               <div
-                className="select-none rounded-full bg-gray-100  hover:bg-[#8C8F99] cursor-pointer flex justify-center items-center h-8 w-8 md:w-12 md:h-12 duration-150 text-center"
+                className="select-none rounded-full bg-gray-100 hover:bg-[#8C8F99] cursor-pointer flex justify-center items-center h-8 w-8 md:w-12 md:h-12 duration-150 text-center"
                 onClick={(e) =>
                   e.stopPropagation() || instanceRefSecond.current?.next()
                 }

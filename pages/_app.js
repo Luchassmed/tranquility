e2fb3c7a12163f1e81d552154b2f3d0880462427
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2023-01-03 14:00:06 +0100

    feat: gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-K73288L6RY"
+    strategy="afterInteractive"
+  >
+    {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K73288L6RY');
+  `}
+  </Script>;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

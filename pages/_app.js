eae14055a3bc630105715479932fb9e2211f02be
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2023-01-03 14:05:53 +0100

    refac: gtag

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,24 @@ import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-K73288L6RY"
-    strategy="afterInteractive"
-  >
-    {` window.dataLayer = window.dataLayer || [];
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-K73288L6RY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {` 
+    window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-K73288L6RY');
   `}
-  </Script>;
-  return <Component {...pageProps} />;
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
